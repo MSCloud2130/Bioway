cfcb529ffd1f27350cb3aa37c75e48427a67acc0
Juan-C-Suarez <juancarlossuarezj@gmail.com>
2021-09-21 06:33:54 -0500

    Proyecto de microservicio de productos creado.

---
+++ Source/products/src/test/java/com/omega/bioway/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omega.bioway.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
