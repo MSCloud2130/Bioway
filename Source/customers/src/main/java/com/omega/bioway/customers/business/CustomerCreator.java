b2ae981a3305b1fe691d36440cd9ebf3112ac453
SantiagoCaroprese <66383253+SantiagoCaroprese@users.noreply.github.com>
2021-09-26 14:57:54 -0500

    postman collection updated

--- Source/customers/src/main/java/com/omega/bioway/customers/business/CustomerCreator.java
+++ Source/customers/src/main/java/com/omega/bioway/customers/business/CustomerCreator.java
@@ -1,7 +1,6 @@
 package com.omega.bioway.customers.business;
 
 import com.omega.bioway.customers.crosscutting.entities.Customer;
-import com.omega.bioway.customers.crosscutting.exceptions.CustomerAlreadyExist;
 import com.omega.bioway.customers.dataaccess.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
