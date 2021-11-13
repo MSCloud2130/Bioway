22b214f0f59950e43771e1bdd2d27852e6c17c94
Camilo Buitrago <camilobl1999@gmail.com>
2021-11-12 22:06:16 -0500

    modificacion archivo seguridad

--- Source/apigateway/src/main/java/com/omega/bioway/apigateway/security/WebSecurityConfig.java
+++ Source/apigateway/src/main/java/com/omega/bioway/apigateway/security/WebSecurityConfig.java
@@ -29,6 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET,"/products/{id}").permitAll()
                 .antMatchers(HttpMethod.POST,"/products/{productId}/questions").hasAnyAuthority(CUSTOMER)
                 .antMatchers(HttpMethod.PUT,"/products/{productId}/questions").hasAnyAuthority(SUPPLIER)
+                .antMatchers(HttpMethod.POST,"/products/{productId}/ratings").hasAnyAuthority(CUSTOMER)
+                .antMatchers(HttpMethod.DELETE,"/products/{productId}").hasAnyAuthority(SUPPLIER)
                 .antMatchers(HttpMethod.POST,"/products").hasAnyAuthority(SUPPLIER)
                 .antMatchers(HttpMethod.PUT,"/products/{productId}").hasAnyAuthority(SUPPLIER)
                 .antMatchers(HttpMethod.POST, "/payments").hasAnyAuthority(CUSTOMER)
